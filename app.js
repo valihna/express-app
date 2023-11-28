b06e3196a2b86cd2dedf8639a722c2b689d52e1b
Valeriane SUDRE CHEVALIER <valeriane.chevalier@gmail.com>
2023-11-28 16:07:38 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require("express");
+
+const app = express();
+
+const port = 5000;
+
+const movies = [
+  {
+    id: 1,
+    title: "Citizen Kane",
+    director: "Orson Wells",
+    year: "1941",
+    colors: false,
+    duration: 120,
+  },
+  {
+    id: 2,
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+    year: "1972",
+    colors: true,
+    duration: 180,
+  },
+  {
+    id: 3,
+    title: "Pulp Fiction",
+    director: "Quentin Tarantino",
+    year: "1994",
+    color: true,
+    duration: 180,
+  },
+];
+
+const getMovies = (req,res) => {
+  res.status(200).json(movies);
+}
+const moviesList = (req, res)=>{
+  res.send("Welcome to my favourite movie list");
+}
+
+app.get("/",moviesList);
+// app.get("/movies/:id",getMovies);
+
+app.get("/api/movies", getMovies);
+
+app.get('/api/movies/:id', (req, res) => {
+  const movieId = parseInt(req.params.id);
+  const movie = movies.find((m) => m.id === movieId);
+
+  if (movie) {
+      res.status(200).json(movie);
+  } else {
+      res.status(404).json({ message: 'Not Found' });
+  }
+});
+
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
